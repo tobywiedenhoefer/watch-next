d77d377642254bea2becd78ff3dbfc01644beef7
toby.wiedenhoefer <toby.wiedenh@gmail.com>
2023-08-25 11:38:31 -0500

    added username to user context

--- src/firebase.tsx
+++ src/firebase.tsx
@@ -1,6 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+import { doc, getDoc, getFirestore } from "firebase/firestore";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 import UserState from "./shared/types/userstate.type";
@@ -10,13 +10,22 @@ const firebaseConfig = {
 
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+export const db = getFirestore(app);
 export const onAuthStateChange = (callback: React.Dispatch<React.SetStateAction<UserState>>) => {
-  return auth.onAuthStateChanged(user => {
+  return auth.onAuthStateChanged(async user => {
+    let username = ""
     const loggedIn: boolean = user != null
+    if (loggedIn) {
+      const docRef = doc(db, "usernames", user!.uid);
+      const docSnap = await getDoc(docRef);
+      const docData = docSnap.data()
+      username = docData ? docData.name : ""
+    }
     const state: UserState = { 
       loggedIn: loggedIn,
       email: loggedIn ? user!.email : "",
-      uid: loggedIn ? user!.uid : ""
+      uid: loggedIn ? user!.uid : "",
+      username: username
     }
     callback(state)
   })
